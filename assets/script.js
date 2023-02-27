d9436b2636cfc8ead68785e92e83a6e5880ce29b
Valentin Mangini <mangini.valentin@gmail.com>
2023-02-27 18:23:14 +0100

    Actualisation fleches, diapo image

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,24 @@
+const pathImg = "./assets/images/slideshow/";
+
 const slides = [
   {
-    image: "slide1.jpg",
+    image: `${pathImg}slide1.jpg`,
     tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
   },
   {
-    image: "slide2.jpg",
+    image: `${pathImg}slide2.jpg`,
     tagLine: "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
   },
   {
-    image: "slide3.jpg",
+    image: `${pathImg}slide3.jpg`,
     tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
   },
   {
-    image: "slide4.png",
+    image: `${pathImg}slide4.png`,
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
+
 //*TABLEAU*//
 slides.length;
 console.log(slides.length);
@@ -27,15 +30,46 @@ for (let i = 0; i < slides.length; i++) {
 
 //*ARROW *//
 const bannerImg = document.querySelector(".banner-img");
+const tagLine = document.querySelector(".tagLine");
 const arrow_right = document.querySelector(".arrow_right");
 const arrow_left = document.querySelector(".arrow_left");
 
+let current = 0;
+
 arrow_right.addEventListener("click", function () {
-  alert("clique");
+  if (current === slides.length - 1) {
+    dot[currentIndex].classList.remove("dot_selected");
+    currentIndex = 0;
+    dot[currentIndex].classList.add("dot_selected");
+  } else {
+    dot[currentIndex].classList.remove("dot_selected");
+    currentIndex++;
+    dot[currentIndex].classList.add("dot_selected");
+  }
+  current++;
+
+  bannerImg.src = slides[currentIndex].image;
+  tagLine.innerHTML = slides[currentIndex].tagLine;
 });
 
 arrow_left.addEventListener("click", function () {
-  alert("bonjour");
+  if (current === 0) {
+    dot[currentIndex].classList.remove("dot_selected");
+    currentIndex = slides.length - 1;
+    dot[currentIndex].classList.add("dot_selected");
+  } else {
+    dot[currentIndex].classList.remove("dot_selected");
+    currentIndex--;
+    dot[currentIndex].classList.add("dot_selected");
+  }
+  current--;
+
+  bannerImg.src = slides[currentIndex].image;
+  tagLine.innerHTML = slides[currentIndex].tagLine;
+
+  if (current < 0) {
+    current = slides.length - 1;
+  }
 });
 
 //* DOTS *//
@@ -50,14 +84,15 @@ for (let i = 0; i < slides.length; i++) {
 const dot = document.querySelectorAll(".dot");
 const dot_selected = document.querySelector(".dot_selected");
 
-dot[currentIndex].classList.add(".dot_selected");
-
 let currentIndex = 0;
 
+dot[currentIndex].classList.add("dot_selected");
+
 function setCurrentSlide(index) {
-  dot[currentIndex].classList.remove(".dot_selected");
+  dot[currentIndex].classList.remove("dot_selected");
   currentIndex = index;
-  dot[currentIndex].classList.add(".dot_selected");
+  dot[currentIndex].classList.add("dot_selected");
+  current = index;
 }
 
 for (let i = 0; i < slides.length; i++) {
